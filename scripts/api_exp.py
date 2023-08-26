bd07e01e47843346875c712805c2c0e7db66f060
vr <55696735+vr18ub@users.noreply.github.com>
2023-08-26 16:13:14 +0200

    change

--- scripts/api_exp.py
+++ scripts/api_exp.py
@@ -1,7 +1,9 @@
 import openai
 import tiktoken
+from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
 
-from scripts.util import average_bert_score, average_rouge_scores, export_output, get_val_dataset, truncate_text, number_of_tokens, tokenize
+from scripts.util import average_bert_score, average_rouge_scores, export_output, get_val_dataset, \
+    truncate_text, number_of_tokens, tokenize, deepinfra_create, setup_logger, prepare_dataset
 
 import os
 from nltk.translate import meteor_score
@@ -20,17 +22,6 @@ import time
 import datetime
 
 ### Initialization
-def setup_logger():
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
-
-    return logger
 
 def log_test_scores(meteor_score_avg, rouge_score_avg, bleu_score_avg, bert_score_avg):
     wandb.log({
@@ -56,7 +47,7 @@ def log_test_scores(meteor_score_avg, rouge_score_avg, bleu_score_avg, bert_scor
     logger.info(f"Average BERTScore: {bert_score_avg}")
     print()
 
-def compute_scores(completion_dataset, num_examples=100):
+def compute_scores(completion_dataset, num_examples=10):
     scores = {'meteor': [], 'rouge': [], 'bleu': [], 'bert': []}
     rouge = Rouge()
     bertscore = load("bertscore")
@@ -132,28 +123,6 @@ def compute_scores(completion_dataset, num_examples=100):
     return np.mean(scores['meteor']), average_rouge_scores(scores['rouge']), np.mean(
         scores['bleu']), average_bert_score(scores['bert'])
 
-def prepare_dataset(dataset, input_len, output_len, sum, origin):
-    """
-       if args.origin is "True": it takes origin_facts and origin_considerations as input
-       else: it takes facts and considerations as input except specific input_col_name and target_col_name are given as arguments
-       """
-    if sum == "True":
-        input_col_name = "text"
-        target_col_name = "regeste"
-    if sum == "False":
-        input_col_name = "facts"
-        target_col_name = "considerations"
-    if origin == "True":
-        raise NotImplementedError("Origin not implemented yet")
-
-    data_list = [{"input": i, "target": t, "lang": lang} for i, t, lang in zip(dataset[input_col_name], dataset[target_col_name], dataset["language"])]
-
-    # truncate input and target to input_len and output_len words:
-    # but join input and target first, so that we can truncate them together
-    for i in range(len(data_list)):
-        data_list[i]["input"] = truncate_text(data_list[i]["input"], input_len, model_name)
-        data_list[i]["target"] = truncate_text(data_list[i]["target"], output_len, model_name)
-
 
     # we want to return a list of dicts with the keys "input" and "target"
     return data_list
@@ -164,42 +133,66 @@ def create_instruction(input, lang, task):
     """
     if task == "cvg":
         if lang == 'de':
-            instruction = f'Ziel: Generiere Erwägungen basierend auf dem gegebenen Sachverhalt eines Schweizer Gerichtsurteils.\nHintergrund: Ein Schweizer Gerichtsurteil besteht aus Rubrum, Sachverhalt, Erwägungen, Dispositiv (Urteilsformel) und Unterschrift. Die Erwägungen sind die rechtliche Würdigung des Geschehens durch das Gericht.\nAnweisung:\n-Sachverhalt Verstehen: Der gegebene Sachverhalt enthält bestrittene und unbestrittene Fakten, die Begehren der Parteien, das Beweisverfahren und die Prozessgeschichte.\n-Beginne mit Prozessvoraussetzungen: Prüfe zunächst, ob die Prozessvoraussetzungen (z.B. Zuständigkeit des Gerichts) erfüllt sind. Wenn nicht strittig, reicht es aus zu bestätigen, dass die Voraussetzungen erfüllt sind.\n-Rechtliche Würdigung:\nEruieren Sie relevante Rechtssätze basierend auf den behaupteten und rechtlich relevanten Tatsachen.\n-Setzen Sie sich mit den rechtlichen Standpunkten der Parteien auseinander.\n-Beachten Sie die Beweislastverteilung und würdigen Sie die Beweise frei, aber berücksichtigen Sie relevante gesetzliche Beweisregeln.\n-Iura novit curia: Ihre rechtliche Würdigung muss nicht zwangsläufig dem rechtlichen Vorbringen der Parteien entsprechen. Berücksichtigen Sie andere mögliche Argumentationslinien.\n-Zusammenfassung: Fassen Sie am Ende Ihrer Erwägungen das Ergebnis Ihrer rechtlichen Würdigung zusammen.\n-Output: Der generierte Text sollte strukturiert, klar und in der Form von typischen Erwägungen eines Schweizer Gerichtsurteils sein.\n\nSachverhalt des Schweizer Gerichtsurteils:\n\n{input}\n\nErwägungen:\n\n'
+            instruction = f'Ziel: Generiere Erwägungen basierend auf dem gegebenen Sachverhalt eines Schweizer Gerichtsurteils.\nHintergrund: Ein Schweizer Gerichtsurteil besteht aus Rubrum, Sachverhalt, Erwägungen, Dispositiv (Urteilsformel) und Unterschrift. Die Erwägungen sind die rechtliche Würdigung des Geschehens durch das Gericht.\nAnweisung:\n-Sachverhalt Verstehen: Der gegebene Sachverhalt enthält bestrittene und unbestrittene Fakten, die Begehren der Parteien, das Beweisverfahren und die Prozessgeschichte.\n-Beginne mit Prozessvoraussetzungen: Prüfe zunächst, ob die Prozessvoraussetzungen (z.B. Zuständigkeit des Gerichts) erfüllt sind. Wenn nicht strittig, reicht es aus zu bestätigen, dass die Voraussetzungen erfüllt sind.\n-Rechtliche Würdigung:\nEruiere relevante Rechtssätze basierend auf den behaupteten und rechtlich relevanten Tatsachen.\n-Setze dich mit den rechtlichen Standpunkten der Parteien auseinander.\n-Beachte die Beweislastverteilung und würdige die Beweise frei, aber berücksichtige relevante gesetzliche Beweisregeln.\n-Iura novit curia: Deine rechtliche Würdigung muss nicht zwangsläufig dem rechtlichen Vorbringen der Parteien entsprechen. Berücksichtige andere mögliche Argumentationslinien.\n-Zusammenfassung: Fasse am Ende deine Erwägungen, das Ergebnis Ihrer rechtlichen Würdigung, zusammen.\n-Output: Der generierte Text sollte strukturiert, klar und in der Form von typischen Erwägungen eines Schweizer Gerichtsurteils sein.\n\nSachverhalt des Schweizer Gerichtsurteils:\n\n{input}\n\nErwägungen:\n\n'
         elif lang == 'fr':
-            instruction = f"But: Générer des considérations basées sur les faits donnés d'un jugement suisse.\nContexte: Un jugement suisse est composé du rubrum, des faits, des considérations, du dispositif (formule du jugement) et de la signature. Les considérations sont l'appréciation juridique des événements par le tribunal.\nInstructions:\n- Comprendre les faits: Les faits donnés contiennent des faits contestés et non contestés, les demandes des parties, la procédure de preuve et l'historique du procès.\n- Commencer par les conditions de procédure: Vérifiez d'abord si les conditions de procédure (par exemple, la compétence du tribunal) sont remplies. Si cela n'est pas contesté, il suffit de confirmer que les conditions sont remplies.\n- Appréciation juridique:\nÉvaluez les dispositions juridiques pertinentes basées sur les faits allégués et juridiquement pertinents.\n- Confrontez-vous aux points de vue juridiques des parties.\n- Tenez compte de la répartition de la charge de la preuve et évaluez les preuves librement, mais tenez compte des règles légales de preuve pertinentes.\n- Iura novit curia: Votre appréciation juridique ne doit pas nécessairement correspondre aux arguments juridiques présentés par les parties. Considérez d'autres lignes d'argumentation possibles.\n- Résumé: Résumez à la fin de vos considérations le résultat de votre appréciation juridique.\n- Résultat: Le texte généré devrait être structuré, clair et sous la forme de considérations typiques d'un jugement suisse.\n\nFaits du jugement suisse:\n\n{input}\n\nConsidérations:\n\n"
+            instruction = f"But: Génère des considérations basées sur les faits donnés d'un jugement suisse.\nContexte: Un jugement suisse est composé du rubrum, des faits, des considérations, du dispositif (formule du jugement) et de la signature. Les considérations sont l'appréciation juridique des événements par le tribunal.\nInstructions:\n- Comprends les faits: Les faits donnés contiennent des faits contestés et non contestés, les demandes des parties, la procédure de preuve et l'historique du procès.\n- Commence par les conditions de procédure: Vérifie d'abord si les conditions de procédure (par exemple, la compétence du tribunal) sont remplies. Si cela n'est pas contesté, il suffit de confirmer que les conditions sont remplies.\n- Appréciation juridique:\nÉvalue les dispositions juridiques pertinentes basées sur les faits allégués et juridiquement pertinents.\n- Confronte-toi aux points de vue juridiques des parties.\n- Tiens compte de la répartition de la charge de la preuve et évalue les preuves librement, mais tiens compte des règles légales de preuve pertinentes.\n- Iura novit curia: Ton appréciation juridique ne doit pas nécessairement correspondre aux arguments juridiques présentés par les parties. Considère d'autres lignes d'argumentation possibles.\n- Résumé: Résume à la fin de tes considérations le résultat de ton appréciation juridique.\n- Résultat: Le texte généré devrait être structuré, clair et sous la forme de considérations typiques d'un jugement suisse.\n\nFaits du jugement suisse:\n\n{input}\n\nConsidérations:\n\n"
         elif lang == 'it':
-            instruction = f"Obiettivo: Generare considerazioni basate sui fatti presentati in una sentenza svizzera.\nContesto: Una sentenza svizzera si compone di rubrum, fatti, considerazioni, dispositivo (formula della sentenza) e firma. Le considerazioni rappresentano la valutazione giuridica degli eventi da parte del tribunale.\nIstruzioni:\n- Comprendere i fatti: I fatti presentati includono fatti contestati e non contestati, le richieste delle parti, la procedura probatoria e la storia del processo.\n- Iniziare con le condizioni processuali: Verificare prima di tutto se le condizioni processuali (ad es. la competenza del tribunale) sono soddisfatte. Se non contestate, è sufficiente confermare che le condizioni sono state soddisfatte.\n- Valutazione giuridica:\nValutare le norme giuridiche rilevanti in base ai fatti affermati e giuridicamente rilevanti.\n- Confrontarsi con i punti di vista giuridici delle parti.\n- Tenere conto della distribuzione dell'onere della prova e valutare le prove liberamente, ma considerare le regole di prova legalmente rilevanti.\n- Iura novit curia: La tua valutazione giuridica non deve necessariamente corrispondere alle argomentazioni giuridiche delle parti. Considera altre possibili linee di argomentazione.\n- Riassunto: Riassumere alla fine delle tue considerazioni il risultato della tua valutazione giuridica.\n- Risultato: Il testo generato dovrebbe essere strutturato, chiaro e nella forma di considerazioni tipiche di una sentenza svizzera.\n\nFatti della sentenza svizzera:\n\n{input}\n\nConsiderazioni:\n\n"
+            instruction = f"Obiettivo: Genera considerazioni basate sui fatti presentati in una sentenza svizzera.\nContesto: Una sentenza svizzera si compone di rubrum, fatti, considerazioni, dispositivo (formula della sentenza) e firma. Le considerazioni rappresentano la valutazione giuridica degli eventi da parte del tribunale.\nIstruzioni:\n- Comprendi i fatti: I fatti presentati includono fatti contestati e non contestati, le richieste delle parti, la procedura probatoria e la storia del processo.\n- Inizia con le condizioni processuali: Verifica prima di tutto se le condizioni processuali (ad es. la competenza del tribunale) sono soddisfatte. Se non contestate, basta confermare che le condizioni sono state soddisfatte.\n- Valutazione giuridica:\nValuta le norme giuridiche rilevanti in base ai fatti affermati e giuridicamente rilevanti.\n- Confrontati con i punti di vista giuridici delle parti.\n- Tieni conto della distribuzione dell'onere della prova e valuta le prove liberamente, ma considera le regole di prova legalmente rilevanti.\n- Iura novit curia: La tua valutazione giuridica non deve necessariamente corrispondere alle argomentazioni giuridiche delle parti. Considera altre possibili linee di argomentazione.\n- Riassunto: Riassumi alla fine delle tue considerazioni il risultato della tua valutazione giuridica.\n- Risultato: Il testo generato dovrebbe essere strutturato, chiaro e nella forma di considerazioni tipiche di una sentenza svizzera.\n\nFatti della sentenza svizzera:\n\n{input}\n\nConsiderazioni:\n\n"
     elif task == 'summ':
-        instruction = f"Ziel: Generiere eine Regeste basierend auf dem gegebenen Sachverhalt, Erwägungen und Dispositiv eines Schweizer Gerichtsurteils.\nHintergrund: Ein Schweizer Gerichtsurteil besteht aus Sachverhalt, Erwägungen und Dispositiv. Die Regeste dient als Kurzzusammenfassung einer gerichtlichen Entscheidung mit Leitsätzen.\nAnweisung:\n1. Sachverhalt: Überprüfe den Sachverhalt.\n2. Erwägungen: Analysiere die Erwägungen.\n3. Dispositiv: Berücksichtige das Dispositiv.\n4. Erstelle die Regeste: Fasse den Fall in Leitsätzen zusammen.\nOutput: Die Regeste sollte eine klare Kurzzusammenfassung mit Leitsätzen bieten.\n\nGegebener Sachverhalt, Erwägungen und Dispositiv:\n\n{input}\n\nGeneriere nun die kurze Regeste.\n\nRegeste:\n\n"
+        instruction = f"Ziel: Generiere eine Regeste basierend auf einem Schweizer Gerichtsurteils.\nHintergrund: Ein Schweizer Gerichtsurteil setzt sich aus Sachverhalt, Erwägungen und Dispositiv zusammen. Die Regeste dient als Kurzzusammenfassung und beinhaltet Leitsätze des Urteils. Nur Leitentscheide haben eine Regeste.\nAnweisung:\n1. Sachverhalt: Lies und verstehe den gegebenen Sachverhalt.\n2. Erwägungen: Analysiere die Erwägungen, um die Hauptargumente und Gründe zu identifizieren.\n3. Dispositiv: Beachte das Dispositiv, da es das endgültige Urteil enthält.\n4. Erstelle die Regeste: Die Regeste sollte aus drei sehr kurzen Teilen bestehen: a. Zitiere die wichtigsten relevanten Artikelziffern (ohne den Artikeltitel). b. Nenne kurze, relevante, deskriptive Keywords, über die Thematik des Falls. c. Formuliere einen sehr kurzen Fliesstext, der die wichtigsten Erwägungen zitiert und kurz zusammenfasst.\nOutput: Die Regeste sollte eine klare und strukturierte Kurzzusammenfassung des Urteils bieten, die aus zitierten Artikeln, Keywords und einem sehr kurzen Fliesstext besteht.\n\nGegebener Sachverhalt, Erwägungen und Dispositiv:\n\n{input}\n\nGeneriere nun die Regeste auf Deutsch:\n\n"
 
     else:
         raise NotImplementedError("Task not implemented yet")
     return instruction
 
-def generate_completions(dataset, input_length, output_length, model):
+def generate_completions(dataset, input_length, output_length, model, api):
     openai.api_key = os.getenv("OPENAI_API_KEY")
     dataset_with_predicted = []
     logger.info("Using model: " + model)
-    if len(dataset) > 50:
-        input("Are you sure you want to generate completions for " + str(len(dataset)) + " examples? Press Enter to continue...")
-    if len(dataset) > 100:
-        input("Are you really sure? Press Enter to continue...")
-
     for entry in dataset:
         instruct = create_instruction(entry["input"], entry["lang"], task_name)
         # measure time to generate completion
         start = time.time()
-        completion = openai.ChatCompletion.create(
-            model=model,
-            messages=[
-                {"role": "user", "content": instruct}
-                ],
-            max_tokens=output_length,
-            )
+        if api == "openai":
+            completion = openai.ChatCompletion.create(
+                model=model,
+                messages=[
+                    {"role": "user", "content": instruct}
+                    ],
+                max_tokens=output_length,
+                )
+            predicted = completion.choices[0].message["content"]
+
+        elif api == "claude":
+            anthropic = Anthropic()
+            completion = anthropic.completions.create(
+                model=model_name,
+                prompt=f"{HUMAN_PROMPT} {instruct}{AI_PROMPT}",
+                max_tokens_to_sample=output_length,
+                )
+            predicted = completion.completion
+        elif api == "deepinfra":
+            completion = deepinfra_create(
+                model_name=model_name,
+                prompt=f"<<SYS>>\n{instruct}\n<</SYS>>\n\n",
+                max_tokens=output_length,
+                )
+            predicted = completion
+        else:
+            raise NotImplementedError("API not implemented yet")
+
         end = time.time()
-        logger.info("Time to generate completion: " + str(end - start))
-        print(completion)
-        predicted = completion.choices[0].message["content"]
+        completion_time = end - start
+        # if model is gpt-4: wait, so that we don't exceed 10k tokens per minute: calculate using input length, output length and copmletion time
+        if model == "gpt-4":
+            # to not exceed rate limit of 10k tokens per minute
+            time_to_wait = (input_length + output_length) / 10000 * 60 - completion_time
+            logger.info("Time to wait: " + str(time_to_wait))
+            time.sleep(time_to_wait)
+
+
+        logger.info("Time to generate completion: " + str(completion_time))
+        print("predicted: ", predicted)
         entry_with_predicted = {"input": entry["input"], "target": entry["target"], "predicted": predicted, "lang": entry["lang"]}
         dataset_with_predicted.append(entry_with_predicted)
 
@@ -216,6 +209,7 @@ logger = setup_logger()
 parser = argparse.ArgumentParser()
 parser.add_argument("--finetune", help="Want to finetune model?")
 parser.add_argument("--model", help="Model name for finetune / evaluation (depends on finetune flag")
+parser.add_argument("--api", help="Which api to generate completions: openai, claude, deepinfra, vertex")
 parser.add_argument("--train_size", help="Size of training set", type=int)
 parser.add_argument("--eval_size", help="Size of evaluation set", type=int)
 parser.add_argument("--test_size", help="Size of test set", type=int)
@@ -229,6 +223,7 @@ parser.add_argument("--sum", help="Loads summarization dataset if True")
 args = parser.parse_args()
 
 model_name = args.model
+api_name = args.api
 
 if args.origin == "True" and args.sum == "True":
     raise ValueError("Cannot use both origin and sum flags (as True)")
@@ -278,10 +273,10 @@ wandb.config.update(args)
 output_examples = []
 
 # prepare dataset for generation
-eval_data = prepare_dataset(eval_dataset, args.input_length, args.output_length, args.sum, args.origin)
+eval_data = prepare_dataset(eval_dataset, args.input_length, args.output_length, args.sum, args.origin, model_name)
 
 # generate output examples using API completions
-completion_dataset = generate_completions(eval_data, args.input_length, args.output_length, model_name)
+completion_dataset = generate_completions(eval_data, args.input_length, args.output_length, model_name, api_name)
 
 # Evaluate model on test dataset
 meteor_score_avg, rouge_score_avg, bleu_score_avg, bert_score_avg = compute_scores(completion_dataset)
@@ -297,5 +292,9 @@ except Exception as e:
 
 # measure time for whole script
 end = time.time()
+
+# wandb log time per item
+wandb.log({"time_per_item": (end - start) / len(eval_dataset)})
+
 # in readable format
 logger.info("Time for whole script: " + str(datetime.timedelta(seconds=end - start)))
--- scripts/util.py
+++ scripts/util.py
@@ -4,6 +4,12 @@ import json
 import os
 import tiktoken
 
+import requests
+import json
+import os
+
+import logging
+
 
 def get_batch_size(model_type, gpu_memory, input_length, output_length):
     # 2048 is the maximum input length for all models
@@ -180,24 +186,99 @@ def get_val_dataset(logger, sum="False", origin="False"):
             dataset = load_dataset("rcds/swiss_court_view_generation", "main", split="validation")
     return dataset
 
-def truncate_text(text, max_tokens, tokenizer):
+def get_tokenizer(tokenizer):
     enc = tiktoken.get_encoding("cl100k_base")
+    if "gpt" not in tokenizer[:4]:
+        print(f"no score tokenizer for {tokenizer}")
+        tokenizer = "gpt-3.5-turbo"
+    print("Loading tokenizer", tokenizer)
     enc = tiktoken.encoding_for_model(tokenizer)
+    return enc
+
+def truncate_text(text, max_tokens, tokenizer):
+    enc = get_tokenizer(tokenizer)
     text_token_ids = enc.encode(text)
     if len(text_token_ids) > max_tokens:
         text_token_ids = text_token_ids[:max_tokens]
     return enc.decode(text_token_ids)
 
 def number_of_tokens(text, tokenizer):
-    enc = tiktoken.get_encoding("cl100k_base")
-    enc = tiktoken.encoding_for_model(tokenizer)
+    enc = get_tokenizer(tokenizer)
     return len(enc.encode(text))
 
 def tokenize(text, tokenizer):
-    enc = tiktoken.get_encoding("cl100k_base")
-    enc = tiktoken.encoding_for_model(tokenizer)
-    print("loading tokenizer for model: ", tokenizer)
+    enc = get_tokenizer(tokenizer)
     text_token_ids = enc.encode(text)
     # we want a list text_tokens with applying enc.decode([x]) for each x in text_token_ids
     text_tokens = [enc.decode([x]) for x in text_token_ids]
     return text_tokens ## list of tokens
+
+
+def deepinfra_create(model_name, prompt, max_tokens):
+    # Get the authorization token from environment variable
+    auth_token = os.environ.get("DINFRA_API_KEY")
+    if not auth_token:
+        raise ValueError("Authorization token not set in environment variable DINFRA_API_KEY")
+    # Endpoint and headers
+    url = f'https://api.deepinfra.com/v1/inference/{model_name}'
+    print(f"using model: {model_name}")
+    headers = {
+        'Authorization': f'bearer {auth_token}',
+        'Content-Type': 'application/json'
+        }
+    # Data to send
+    data = {
+        'input': prompt,
+        'max_new_tokens': max_tokens
+        }
+
+    # Make the POST request
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    # Handle potential errors (you can expand on this if needed)
+    if response.status_code != 200:
+        raise Exception(f"API call failed with status {response.status_code}: {response.text}")
+
+    # Extract generated text
+    json_response = response.json()
+    if 'results' in json_response and len(json_response['results']) > 0 and 'generated_text' in \
+            json_response['results'][0]:
+        print("json response", json_response)
+        return json_response['results'][0]['generated_text']
+    else:
+        raise Exception("Generated text not found in the API response.")
+
+
+def setup_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    return logger
+
+def prepare_dataset(dataset, input_len, output_len, sum, origin, model_name):
+    """
+    -Based on the type of dataset (sum or cvg), it returns a dict with the right input and target columns
+    -Then it truncates the input and target to input_len and output_len tokens (using the tokenizer of the model)
+    """
+    if sum == "True":
+        input_col_name = "text"
+        target_col_name = "regeste"
+    if sum == "False":
+        input_col_name = "facts"
+        target_col_name = "considerations"
+    if origin == "True":
+        raise NotImplementedError("Origin not implemented yet")
+
+    data_list = [{"input": i, "target": t, "lang": lang} for i, t, lang in zip(dataset[input_col_name], dataset[target_col_name], dataset["language"])]
+
+    # truncate input and target to input_len and output_len words:
+    # but join input and target first, so that we can truncate them together
+    for i in range(len(data_list)):
+        data_list[i]["input"] = truncate_text(data_list[i]["input"], input_len, model_name)
+        data_list[i]["target"] = truncate_text(data_list[i]["target"], output_len, model_name)
